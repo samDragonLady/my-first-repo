c28de26f9f6a335641dfef9ed419d29e67ee8187
Sam Smith-Anderson <saraamandamarie@gmail.com>
2023-02-04 13:53:14 -0800

    updated home page and css

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var name = "Jason";
 var greeting = "Hello World!";
-<<<<<<< HEAD
 var hobby = "skateboarding";
-=======
-var age = "25;"
->>>>>>> 6a18278d4336ebeeb8cf31bd0147febbcf342f83
+var age = "25";
